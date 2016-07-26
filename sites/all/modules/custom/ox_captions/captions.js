bb7b255f4131b259afea4e7559948bf444d55218
vagrant <vagrant@pod-dev.dev>
2016-07-26 12:28:27 +0000

    WIP: Add ox_captions module
    Place to develop captionings, if necessary.
    Some of this has been re-factored from
    ox_jwplayer module.

---
+++ sites/all/modules/custom/ox_captions/captions.js
@@ -0,0 +1,34 @@
+(function ($) {
+  Drupal.behaviors.Captions = {
+    attach: function (context, settings) {
+      // Code to be run on page load, and
+      // on ajax load added here
+      // 
+            var playerInstance = jwplayer("jwplayer8116jwplayer-episode-node")
+
+            playerInstance.setup({
+                playlist: [{
+                    file: "http://media.podcasts.ox.ac.uk//comlab/comsci/lovelace/2015-12-09-acm-lovelace-session-1-intro-wolf.mp4",  
+                    tracks: [{ 
+                        file: "/sites/default/files/2015-12-09-acm-lovelace-session-1-intro-wolf.srt", 
+                        label: "English",
+                        kind: "captions",
+                        "default": true,
+
+                    }]
+                }]
+            });
+      // 
+      // 
+      // 
+      // 
+    }
+  };
+} (jQuery));
+
+
+
+
+
+
+
