d4f1443c2913e0a2d157dfda8a058b96b1e7e262
sstose <sstose@gmail.com>
2016-08-31 11:58:56 +0000

    Update WIP captions.js file (slight tweak)

--- sites/all/modules/custom/ox_captions/captions.js
+++ sites/all/modules/custom/ox_captions/captions.js
@@ -4,13 +4,14 @@
       // Code to be run on page load, and
       // on ajax load added here
       // 
-            var playerInstance = jwplayer("jwplayer8116jwplayer-episode-node")
+          var playerInstance = jwplayer("jwplayer8116jwplayer-episode-node")
 
             playerInstance.setup({
                 playlist: [{
                     file: "http://media.podcasts.ox.ac.uk//comlab/comsci/lovelace/2015-12-09-acm-lovelace-session-1-intro-wolf.mp4",  
                     tracks: [{ 
-                        file: "/sites/default/files/2015-12-09-acm-lovelace-session-1-intro-wolf.srt", 
+                        file: "/sites/default/files/2015-12-09-acm-lovelace-session-1-intro-wolf.srt",
+                        // file: "http://media.podcasts.ox.ac.uk//comlab/comsci/lovelace/2015-12-09-acm-lovelace-session-1-intro-wolf.srt", 
                         label: "English",
                         kind: "captions",
                         "default": true,
@@ -25,7 +26,6 @@
     }
   };
 } (jQuery));
-
 
 
 
