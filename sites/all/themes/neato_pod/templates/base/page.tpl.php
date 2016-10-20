6158dc92af13e0b938179703b0e2b2fa46709de8
sstose <sstose@gmail.com>
2016-10-20 14:04:09 +0000

    Major neato_pod templating/grid changes
    Main changes to page.tpl.php and scss

--- sites/all/themes/neato_pod/templates/base/page.tpl.php
+++ sites/all/themes/neato_pod/templates/base/page.tpl.php
@@ -1,88 +1,167 @@
 <div role="document" class="page">
-  <?php if (!empty($page['header'])): ?>
-    <header id="site-header">
-      <div class="outer-wrapper">
-        <?php print render($page['header']); ?>
-      </div>
-    </header>
-  <?php endif; ?>
 
-  <?php if (!empty($page['featured'])): ?>
-    <section id="featured">
-      <div class="outer-wrapper">
-        <?php print render($page['featured']); ?>
-      </div>
-    </section>
-  <?php endif; ?>
+ <!-- ______________________ HEADER _______________________ -->
+  <div role="header" class="header-top outer-wrapper">
+    
+    <div class="no-inner-wrapper">  
+      <?php if (!empty($page['header-top'])): ?>
+        <header id="site-header" class="header-top">
+            <?php print render($page['header-top']); ?>
+        </header>
+      <?php endif; ?>
+    </div>
+  </div>
 
+  <div role="header" class="header-middle outer-wrapper">
+
+    <div class="inner-wrapper">
+      <?php if (!empty($page['header-middle-left'])): ?>
+        <header id="site-header" class="header-middle-left"> 
+            <?php print render($page['header-middle-left']); ?>   
+        </header>
+      <?php endif; ?>
+
+      <?php if (!empty($page['header-middle-right'])): ?>
+        <header id="site-header" class="header-middle-right">  
+            <?php print render($page['header-middle-right']); ?>
+        </header>
+      <?php endif; ?>
+
+    </div>
+    
+  </div>
+
+
+  <div role="header" class="header-bottom outer-wrapper">
+
+    <div class="inner-wrapper">
+      <?php if (!empty($page['header-bottom-left'])): ?>
+        <header id="site-header" class="header-bottom-left"> 
+            <?php print render($page['header-bottom-left']); ?>   
+        </header>
+      <?php endif; ?>
+
+      <?php if (!empty($page['header-bottom-right'])): ?>
+        <header id="site-header" class="header-bottom-right">  
+            <?php print render($page['header-bottom-right']); ?>
+        </header>
+      <?php endif; ?>
+
+    </div>
+    
+  </div>
+    
+
+
+<!-- ______________________ FEATURED _______________________ -->
+  <div class="featured outer-wrapper">
+
+      <?php if (!empty($page['featured'])): ?>
+        <section id="featured" class="no-inner-wrapper">      
+            <?php print render($page['featured']); ?>
+        </section>
+      <?php endif; ?>
+
+  </div>
+<!-- ______________________ ERROR _______________________ -->
+ 
   <?php if ($messages): ?>
     <section id="messages">
-      <div class="outer-wrapper">
+      <div class="inner-wrapper">
         <?php print $messages; ?>
       </div>
     </section>
   <?php endif; ?>
 
-  <?php if ($breadcrumb): ?>
-    <section id="breadcrumb">
-      <div class="outer-wrapper">
-        <?php print $breadcrumb; ?>
-      </div>
-    </section>
-  <?php endif; ?>
+<!-- ______________________ BREADCRUMB _______________________ -->
+  
+  <div class="featured outer-wrapper">
 
-  <main role="main" class="outer-wrapper">
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside id="sidebar-first" role="complementary" class="sidebar">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>
+    <?php if ($breadcrumb): ?>
+      <section id="breadcrumb">
+        <div class="inner-wrapper">
+          <?php print $breadcrumb; ?>
+        </div>
+      </section>
     <?php endif; ?>
 
-    <section id="content">
-      <?php if ($title): ?>
-        <?php print render($title_prefix); ?>
-          <h1 id="page-title"><?php print $title; ?></h1>
-        <?php print render($title_suffix); ?>
+  </div>
+
+
+<!-- ______________________ MAIN _______________________ -->
+
+  <div class="main-wrapper outer-wrapper">
+
+    <main role="main" class="main">
+      <?php if (!empty($page['sidebar_first'])): ?>
+        <aside id="sidebar-first" role="complementary" class="sidebar">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>
       <?php endif; ?>
 
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-        <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+      <section id="content">
+        <?php if ($title): ?>
+          <?php print render($title_prefix); ?>
+            <h1 id="page-title"><?php print $title; ?></h1>
+          <?php print render($title_suffix); ?>
+        <?php endif; ?>
+
+        <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+          <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+        <?php endif; ?>
+
+        <?php if ($action_links): ?>
+          <ul class="action-links">
+            <?php print render($action_links); ?>
+          </ul>
+        <?php endif; ?>
+
+        <?php print render($page['content']); ?>
+      </section>
+
+      <?php if (!empty($page['sidebar_second'])): ?>
+        <aside id="sidebar-second" role="complementary" class="sidebar">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>
       <?php endif; ?>
+    </main>
 
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
+  </div>
+
+
+
+<!-- ______________________ FOOTER _______________________ -->
+
+  <div role="footer" class="footer-top outer-wrapper">
+
+    <div class="inner-wrapper">
+      <?php if (!empty($page['footer_top_left'])): ?>
+        <footer id="footer-top-left" class="footer-top-left" role="contentinfo">
+          <?php print render($page['footer_top_left']); ?>
+        </footer>
       <?php endif; ?>
-
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside id="sidebar-second" role="complementary" class="sidebar">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>
-    <?php endif; ?>
-  </main>
-
-  <?php if (!empty($page['footer_top'])): ?>
-    <footer id="site-footer" role="contentinfo">
-      <?php if (!empty($page['footer_top'])): ?>
-        <section class="footer-top">
-          <?php print render($page['footer_top']); ?>
-        </section>
+  
+      <?php if (!empty($page['footer_top_right'])): ?>
+        <footer id="footer-top-right" class="footer-top-right" role="contentinfo">
+          <?php print render($page['footer_top_right']); ?>
+        </footer>
       <?php endif; ?>
-    </footer>
-  <?php endif; ?>
+    </div>
 
-    <?php if (!empty($page['footer_bottom'])): ?>
-    <footer id="site-footer" role="contentinfo">
+  </div>
+
+  <div role="footer" class="footer-bottom outer-wrapper">
+
+    <div class="no-inner-wrapper">
       <?php if (!empty($page['footer_bottom'])): ?>
-        <section class="footer-bottom">
+        <footer id="footer-bottom" class="footer-bottom" role="contentinfo">
           <?php print render($page['footer_bottom']); ?>
-        </section>
+        </footer>
       <?php endif; ?>
-    </footer>
-  <?php endif; ?>
+    </div>
+
+  </div>
+
+
 </div>
